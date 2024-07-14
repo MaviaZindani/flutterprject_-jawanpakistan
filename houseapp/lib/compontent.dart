aab10544d924a64144245c11803bb6bedd23fde3
MaviaZindani <maviazindaninfo@gmail.com>
2024-07-14 13:08:58 +0500

    House App

---
+++ houseapp/lib/compontent.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color homeBlue = const Color(0xff246bfd);
+
+Color homeGrayBlue = const Color(0xff0F2F44);
+
+Color homeGrayWhite = const Color(0xffEAF1FF);
+
+Color homeTextColor = const Color(0xff0F2F44);
